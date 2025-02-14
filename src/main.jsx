51f1fdf2e85fce4dfbec29d0730d064721204aa8
joylululu <joylululu@gmail.com>
2025-02-14 09:25:51 -0800

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./router";
+//import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
-
-import App from "./App.jsx";
+//import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/React-project-week5/", // 這個一定要加上正確的專案名稱
+  base: "/React-project-week6/", // 這個一定要加上正確的專案名稱
   plugins: [react()],
 });
